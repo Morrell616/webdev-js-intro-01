516b1ad155627946c96cf4fa3bfc680093061473
Jen Morrell <morrell616@hotmail.com>
2024-12-17 16:12:06 -0500

    added new variables

--- js/assignment.js
+++ js/assignment.js
@@ -1,4 +1,4 @@
-"use strict";
+/*"use strict";
 
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
@@ -6,7 +6,9 @@ const bookAnswerEl = document.getElementById("book-answer")
 const activityAnswerEl = document.getElementById("activity-answer")
 const submissionBtn = document.getElementById("submission-btn")
 
+const book = "Christmas Carol";
 
+let activity = "Crafting";
 // Declare a constant variable named "book" and assign
 // the name of your favorite book to that variable.
 
@@ -19,18 +21,20 @@ const submissionBtn = document.getElementById("submission-btn")
 function updateBookSentence() {
     // Sets the text inside the element with the id of book answer. 
     // innerText is one of many properties on a HTML element.
-    bookAnswerEl.innerText = book
+    bookAnswerEl.innerText = book // this links my answer to variable book
 }
 
 function updateActivitySentence() {
+    activityAnswerEl.innerText = activity; 
     // Update this function 
-    // (hint: it will be similiar to the previous function)
-
+    // (hint: it will be similiar to the previous function) 
+    
 }
 
 function render() {
     // Finish writing this function
     updateBookSentence();
+    updateActivitySentence();
 
 }
 
@@ -39,3 +43,4 @@ submissionBtn.addEventListener("click", function () {
     // when the button is clicked
     render();
 })
+*/
